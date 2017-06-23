c973dd0c3fcf146405ad80cedbed5f2779776011
OscarZhou <zhouhongyu1989@gmail.com>
2017-06-24 11:58:48 +1200

    Create the project
    Add the main window

---
+++ OscarPlayer/FrmMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace OscarPlayer
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstItem = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.lbCurrentProgress = new System.Windows.Forms.Label();
+            this.pbVolume = new System.Windows.Forms.ProgressBar();
+            this.chkMute = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lstItem
+            // 
+            this.lstItem.FormattingEnabled = true;
+            this.lstItem.ItemHeight = 18;
+            this.lstItem.Location = new System.Drawing.Point(51, 26);
+            this.lstItem.Name = "lstItem";
+            this.lstItem.Size = new System.Drawing.Size(234, 490);
+            this.lstItem.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(67, 522);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 43);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(185, 522);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 43);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(745, 522);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 43);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(849, 522);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 43);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(640, 522);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 43);
+            this.btnPause.TabIndex = 5;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Location = new System.Drawing.Point(321, 492);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(792, 18);
+            this.pbProgress.TabIndex = 6;
+            // 
+            // lbCurrentProgress
+            // 
+            this.lbCurrentProgress.AutoSize = true;
+            this.lbCurrentProgress.Location = new System.Drawing.Point(1144, 492);
+            this.lbCurrentProgress.Name = "lbCurrentProgress";
+            this.lbCurrentProgress.Size = new System.Drawing.Size(62, 18);
+            this.lbCurrentProgress.TabIndex = 7;
+            this.lbCurrentProgress.Text = "label1";
+            // 
+            // pbVolume
+            // 
+            this.pbVolume.Location = new System.Drawing.Point(1026, 541);
+            this.pbVolume.Name = "pbVolume";
+            this.pbVolume.Size = new System.Drawing.Size(100, 23);
+            this.pbVolume.TabIndex = 8;
+            // 
+            // chkMute
+            // 
+            this.chkMute.AutoSize = true;
+            this.chkMute.Location = new System.Drawing.Point(1147, 541);
+            this.chkMute.Name = "chkMute";
+            this.chkMute.Size = new System.Drawing.Size(79, 22);
+            this.chkMute.TabIndex = 9;
+            this.chkMute.Text = "Mute?";
+            this.chkMute.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 598);
+            this.Controls.Add(this.chkMute);
+            this.Controls.Add(this.pbVolume);
+            this.Controls.Add(this.lbCurrentProgress);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstItem);
+            this.Name = "FrmMain";
+            this.Text = "OPlayer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstItem;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.ProgressBar pbProgress;
+        private System.Windows.Forms.Label lbCurrentProgress;
+        private System.Windows.Forms.ProgressBar pbVolume;
+        private System.Windows.Forms.CheckBox chkMute;
+    }
+}
+
